789d775eddb29b791128ffd8462284c8c84a012a
Achal Bhute <aachalbhute@gmail.com>
2018-03-10 02:09:38 +0530

    added cors

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.listen(PORT, () =>
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-access-token");
+    next();
+  });
 app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
 );
